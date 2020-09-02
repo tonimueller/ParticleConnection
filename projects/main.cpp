a30de6486ee22c4c364df77ab7c813559230a299
Toni Mueller <mueller-toni@ipfdd.de>
2020-09-02 11:04:04 +0200

    extract monomer class into monomer.h

--- projects/main.cpp
+++ projects/main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 #include <vector>
-
-class monomer 
-{
-public:
-  monomer():FreeR(0),N(1),P1(0),P2(0){};
-private:
-  uint8_t FreeR;
-  uint8_t N;
-  uint32_t P1;
-  uint32_t P2;
-};
+#include "./monomers.h"
 
 int main()
 {
